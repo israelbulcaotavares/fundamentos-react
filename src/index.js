e29dfa6a296cd55b8758829810e8dce62850c1aa
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-15 16:58:13 -0300

    Primeiro Componente #01

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import ReactDOM from 'react-dom';
 import React from 'react'
 import './index.css'
 
+import Primeiro from './components/basicos/Primeiro';
+
 
 // const el = document.getElementById('root')
 // ReactDOM.render('Olá', el  )
 
-const tag = <strong>Olá React!</strong> 
+const tag = <strong>Olá React!</strong>
 
 ReactDOM.render(
-     <div>
-        { tag }
-        
-        </div> , 
-    document.getElementById('root')  )
+    <div>
+        <Primeiro />  
+    </div>,
+    document.getElementById('root'))
