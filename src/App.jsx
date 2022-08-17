5b56376460e50b0038beb15cc5a63ca52428d34e
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-17 17:00:19 -0300

    Renderização Condicional #01

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ListaAlunos from "./components/repeticao/ListaAlunos";
 import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import TabelaProdutos from "./components/repeticao/TabelaProdutos";
+import ParOuImpar from "./components/condicional/ParOuImpar";
 
 export default () => {
   return (
@@ -18,6 +19,9 @@ export default () => {
       <h1>Fundamentos React</h1>
 
       <div className="Cards">
+        <Card titulo="#08 - Renderização Condicional" color="#982395">
+          <ParOuImpar numero={20} />
+        </Card>
         <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
           <TabelaProdutos/>
         </Card>
